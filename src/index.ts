55b95ce568cb5666d4cfdb4dbc8ab201c7bfbfce
Shahzer Baig <shahzer.baig.sb@gmail.com>
2024-10-09 20:50:07 +0530

    Changed Port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from TypeScript and Express!");
