fc49d37988de2e9021f7aee8cf0a17adb2effb0d
Shahzer Baig <shahzer.baig.sb@gmail.com>
2024-10-09 21:25:44 +0530

    Added Cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from TypeScript and Express!");
+  res.send({ message: "Hello from TypeScript and Express server!" });
 });
 
 app.listen(port, () => {
